b5421d51280f5ead3049363ec74c1bcf585771b6
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-04-16 07:38:10 +0000

    moved read_output to utils

--- DUST/utils/read_output.py
+++ DUST/utils/read_output.py
@@ -10,8 +10,9 @@ AUTHOR:
 
 
 """
+import pandas as pd
 import xarray as xr
-import pandas as pd 
+
 
 """
 DESCIRPTION:
@@ -57,6 +58,7 @@ USAGE:
         output_dir: Path to directory with containing the output folder
     
         returns: pandas.Dataframe
+
 """
 def read_release_namelist(output_dir):
     relLocactions = pd.DataFrame()
@@ -79,9 +81,20 @@ def read_release_namelist(output_dir):
     return relLocactions
 
 """
+DESCRIPTION
+===========
+
+    Reads the OUTGRID.namelist file in the output directory and  
+    returns the data as python dictionary.
+
 USAGE
 =====
-    
+
+    outGrid = read_outGrid_namelist(output_dir)
+
+        output_dir: path to FLEXPART output directory.
+        returns: python.dictonary 
+ 
 """
 def read_outGrid_namelist(output_dir):
     outGrDict = {}
@@ -103,16 +116,20 @@ def read_outGrid_namelist(output_dir):
         
     return outGrDict
         
-#Need to give Name to columns
+
 """
-      write(unitouttraj,'(i5,i8,2f9.4,4f8.1,f8.2,4f8.1,3f6.1,&
-           &5(2f8.3,f7.0,f6.1,f8.1))')&
-           &j,itime-(ireleasestart(j)+ireleaseend(j))/2, &
-           xcenter,ycenter,zcenter,topocenter,hmixcenter,tropocenter, &
-           pvcenter,rmsdist,rms,zrmsdist,zrms,hmixfract,pvfract, &
-           tropofract, &
-           (xclust(k),yclust(k),zclust(k),fclust(k),rmsclust(k), &
-           k=1,ncluster)
+DESCRIPTION
+===========
+
+    Read in the trajectories.txt and returns the data in a pandas dataframe
+
+USAGE
+=====
+    df = read_trajectories(output_dir, nclusters=5)
+
+        output_dir: Path to directory containing the FLEXPART output
+        nclusters: The number of cluster used in the simulation. (it might be 
+        written in the Trajectories.txt file)
 """
 def read_trajectories(output_dir, nclusters=5):
     cluster_list = []
@@ -129,12 +146,8 @@ def read_trajectories(output_dir, nclusters=5):
          'rms distance', 'rms', 'zrms distance', 'zrms',
          'fraction mixing layer', 'fraction PV<2pvu',
          'fraction in troposphere'] + cluster_list
-    print(len(cols))
-    print(cluster_list)
-    
-    print(len(cols))
-    df = pd.read_csv(output_dir, sep='\s+',
+
+
+    df = pd.read_csv(output_dir + 'trajectories.txt', sep='\s+',
                     skiprows=lambda x: x <24, names=cols)
     return df
-
-        
