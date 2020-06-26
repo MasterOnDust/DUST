7707e9129b2d89c2e1574a36cc0bebe4a291a1e2
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-06-26 08:27:41 +0000

    update

--- calc_conc.py
+++ calc_conc.py
@@ -1,7 +1,7 @@
 import numpy as np
 import xarray as xr
 import pandas as pd
-from dask import delayed
+
 
 """
 Both merging and writing to file want to be done in a parallell 
@@ -23,6 +23,7 @@ def not_usefull(ds):
     return  [v for v in ds.data_vars if v not in essentials]
 
 def pre(ds):
+    # add datetime to record dimension
     ds = ds.assign_coords(record=pd.to_datetime(ds.iedate + ds.ietime))
     return ds.drop(not_usefull(ds))
 
@@ -30,14 +31,14 @@ def pre(ds):
 if __name__ == "__main__":
             
     ds = xr.open_mfdataset('/cluster/projects/nn2806k/ovewh/flexpart/outputs/grid_time_2019*.nc',decode_times=False, 
-                            combine='nested', concat_dim='record', preprocess=pre, parallel=True)
+                            combine='nested', concat_dim='record', preprocess=pre)
     #Subset dataset out of memory
     ds = ds.sel(height=100)
     ds = ds.isel(nageclass=0)
     ds = ds.sel(pointspec=0)
 
     ds = ds.assign_coords(time=pd.to_timedelta(ds.time.values,unit='s'))
-    ems = xr.open_dataarray('/cluster/projects/nn2806k/ovewh/flexpart/dust_2019.nc', chunks={'time':40})
+    ems = xr.open_dataarray('/cluster/projects/nn2806k/ovewh/flexpart/dust_2019.nc')
 
     ds['Concentration'] = multiply_ems(ds,ems)
 
