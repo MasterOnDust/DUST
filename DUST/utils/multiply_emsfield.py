74f04c604c2768a4158bc7b2b33e26faed563f5c
Ove Haugvaldstad <ovewh@outlook.com>
2020-07-29 11:31:20 +0200

    working rutine for multiplying

--- DUST/utils/multiply_emsfield.py
+++ DUST/utils/multiply_emsfield.py
@@ -6,9 +6,11 @@ from dask.delayed import delayed
 import dask
 import numpy as np
 from netCDF4 import Dataset
+from netCDF4 import date2num, num2date
 import os
 import time
 from IPython import embed
+from numba import njit, prange
 
 """
 Description:
@@ -31,85 +33,136 @@ AUTHOR:
 
 """
 
-
 def _setup_netcdf4(path,
-                  locations,
-                  d0):
-    lat = d0.latitude.values
-    lon = d0.longitude.values
-    t = d0.time.values
-    ind_receptor = d0.ind_receptor
-    dims = d0.dims
+                  nc_files, flexdust_path, point_spec):
+    d0 = xr.open_dataset(ncfiles[0], decode_times=False)
+    d0 = sel_location(d0,point_spec)
+       
     try:
         ncfile = Dataset(path, 'w', format="NETCDF4")
     except PermissionError:
         os.remove(path)
-        ncfile = Dataset(path, 'w', format='NETCDF4')
-    groups = []
-    for location in locations:
-        groups.append(ncfile.createGroup("/{}".format(location)))
-    for group in groups:
-        recordDim = group.createDimension('record', None)
-        timeDim = group.createDimension('time', dims['time'])
-        longitudeDim = group.createDimension('lon', dims['longitude'])
-        latitudeDim = group.createDimension('lat', dims['latitude'])
-        lats = group.createVariable('lat', 'f4', ('lat', ))
-        lons = group.createVariable('lon', 'f4', ('lon', ))
-        times = group.createVariable('time', 'i4', ('time',))
-        lons[:] = lon
-        lats[:] = lat
-        times[:] = t
-        s_time = pd.to_datetime(d0.iedate + d0.ietime)
-        record[0] = s_timw
+        ncfile = Dataset(path, 'w', format='NETCDF4') 
+    #Setup attributes
+    ncfile.title = 'Flexpart - Flexdust SSR'
+    ncfile.history = "Created " + time.ctime(time.time())
+    ncfile.flexpart_v = d0.source
+    ncfile.receptor_name = str(d0.RELCOM.values)[2:].strip()
+    ncfile.reference = 'https://doi.org/10.5194/gmd-12-4955-2019, https://doi.org/10.1002/2016JD025482'
 
-        if ind_receptor == 1:
-            field = group.createVariable('Concentration', 'f4',('record', 'time', 'lat', 'lon'))
-            field.units = 'kg/m^3'
-        elif ind_receptor == 4:
-            field = group.createVariable('DryDep', 'f4', ('record', 'time', 'lat', 'lon'))
-            field.units = 'kg/m^2s '
-        elif ind_receptor == 3:
-            field = group.createVariable('WetDep', 'f4', ('record', 'time', 'lat', 'lon'))
-            field.units = 'kg/m^2s '
-        else:
-            field = group.createVariable('Spec_mr', 'f4', ('record', 'time', 'lat', 'lon'))
-            field.units = 'kg/m^3'
-        group.title = 'FLEXPART/FLEXDUST SSR'
-        group.history = "Created " + time.ctime(time.time())
-        group.flexpart_v = d0.source
-        group.reference = 'https://doi.org/10.5194/gmd-12-4955-2019, https://doi.org/10.1002/2016JD025482'
+    
+    
+    lats = d0.latitude.values
+    lons = d0.longitude.values
+    ts = d0.time.values
+    ind_receptor = d0.ind_receptor
+    dims = d0.dims
+    
+
+    #Spatial dims
+    
+    lat_dim = ncfile.createDimension('lat', dims['latitude'])
+    lon_dim = ncfile.createDimension('lon', dims['longitude'])
+    point_dim = ncfile.createDimension('npoint',1)
+    #temporal dims
+    
+    time_dim = ncfile.createDimension('time', None)
+    btime_dim = ncfile.createDimension('btime', dims['time'])
+    
+    
+    #Setup lon/lat (spatial variables)
+    
+    lat = ncfile.createVariable('lat', 'f4', ('lat', ))
+    lat.units = 'degrees_north'
+    lat.long_name = 'latitude'
+    
+    lon = ncfile.createVariable('lon', 'f4', ('lon', ))
+    lon.units = 'degrees_east'
+    lon.long_name = 'longitude'
+    
+    rellat = ncfile.createVariable('RELLAT', 'f4', ('npoint',))
+    rellat.units = 'degrees_north'
+    rellat.long_name = 'latitude_receptor'
+    
+    rellon = ncfile.createVariable('RELLON', 'f4', ('npoint',))
+    rellon.units = 'degrees_east'
+    rellon.long_name = 'longitude_receptor'
+    
+    lon[:] = lons
+    lat[:] = lats
+    
+    rellat[:] = d0.RELLAT1.values
+    rellon[:] = d0.RELLNG1.values
+    
+    
+    # Setup temporal variable 
+    btime = ncfile.createVariable('btime', 'i4', ('btime',))
+    btime.units = 's'
+    btime.long_name = 'seconds_since_release'
+    
+    btime[:] = d0.time.values
+    
+    time_var = ncfile.createVariable('time', 'f8', ('time',))
+    time_var.units = "hours since 1980-01-01"
+    time_var.long_name = 'time'
+    
+    
+    if ind_receptor == 1:
+        field = ncfile.createVariable('Concentration', 'f4',('time', 'btime', 'lat', 'lon'))
+        field.units = 'kg/m^3'
+        field.long_name = 'Concentration'
+    elif ind_receptor == 4:
+        field = ncfile.createVariable('DryDep', 'f4', ('time', 'btime', 'lat', 'lon'))
+        field.units = 'kg/m^2s'
+        field.long_name = 'Dry depostion'
+    elif ind_receptor == 3:
+        field = ncfile.createVariable('WetDep', 'f4', ('time', 'btime', 'lat', 'lon'))
+        field.units = 'kg/m^2s '
+        field.long_name = 'Wet depostion'
+    else:
+        field = ncfile.createVariable('Spec_mr', 'f4', ('time', 'btime', 'lat', 'lon'))
+        field.units = 'kg/m^3'
+    
+    field.height = d0.height.values
+    td = pd.to_timedelta(d0.time.values, unit='s')
+    emsField = DUST.read_flexdust_output(flexdust_path)['dset'].Emission
+    time_steps = []
+    for n, nc_file in enumerate(nc_files):
+        temp_ds = xr.open_dataset(nc_file, decode_times=False)
+        temp_ds = sel_location(temp_ds, point_spec)
+        s_time = pd.to_datetime(temp_ds.iedate + temp_ds.ietime)
+        time_steps.append(date2num(s_time, time_var.units))
+        ems_sens = temp_ds.spec001_mr
+        temp_ems = emsField.sel(time=s_time + td)
+        temp_array = multiply_emissions(temp_ems.values, ems_sens.values)
+        field[n,:,:,:] = temp_array
+    time_var[:] = time_steps[:]
     ncfile.close()
 
+@njit(parallel=True)
+def multiply_emissions(ems, ems_sens):
+    temp_array = np.zeros_like(ems_sens)
+    for i in prange(ems.shape[0]):
+        for j in range(ems.shape[1]):
+            for k in range(ems.shape[2]):
+                temp_array[i,j,k] = ems[i,j,k]*ems_sens[i,j,k]
+    return temp_array
 
-def multiply_emissions(nc_files, flexdust_path, outfile, point_spec):
-    emsField = DUST.read_flexdust_output(flexdust_path)['dset'].Emission
-    for nc_file, n in enumerate(nc_files):
-        ofile = Dataset(outfile, 'a')
-        temp_loc = ofile.groups['SACOL']
-        oVar = temp_loc.variables['DryDep']
-        temp_ds = xr.open_dataset(nc_file, decode_times=False)
-        s_time = pd.to_datetime(temp_ds.iedate + temp_ds.ietime)
-        temp_ds = temp_ds.spec001_mr
-        temp_ds = temp_ds.sel(height=100)
-        temp_ds = temp_ds.sel(nageclass=0)
-        temp_ds = temp_ds.sel(pointspec=point_spec)
-        td = pd.to_timedelta(temp_ds.time.values, unit='s')
-        temp_ems = emsField.sel(time=s_time + td)
-        for i in range(temp_ds.time.shape[0]):
-            for j in range(temp_ds.latitude.shape[0]):
-                for k in range(temp_ds.longitude.shape[0]):
-                    oVar[0,i,j,k] = temp_ems[i,j,k]*temp_ds[i,j,k]/temp_ds.height
-        ofile.close()
-        del oVar
+
+def sel_location(ds,pointspec):
+    ds = ds.sel(pointspec=pointspec)
+    ds = ds.sel(numpoint=pointspec)
+    ds = ds.sel(numspec=pointspec)
+    ds = ds.sel(nageclass=0)
+    ds = ds.sel(height=100)
+    return ds
 
 
 if __name__=="__main__":
 
-    path = '/cluster/work/users/ovewh/accumulated_avg_drydep_test'
+    path = '/mnt/c/Users/oveha/Documents/FLEXPART_output'
 
-    nc_files = glob.glob(path + "/**/*.nc", recursive=True)
-    d0 = xr.open_dataset(nc_files[0])
-    _setup_netcdf4('test.nc', ['SACOL', 'BAODE'], d0)
-    fdp = '/cluster/projects/nn2806k/ovewh/flexdust/outputs/dust_2019_ISRIC/'
-    emsField = DUST.read_flexdust_output(fdp)['dset']
-    multiply_emissions(nc_files, fdp, 'test.nc',0)
+    ncfiles = glob.glob(path + "/**/*.nc", recursive=True)
+
+    _setup_netcdf4('/mnt/c/Users/oveha/Documents/test_parallel.nc', ncfiles, '/mnt/c/Users/oveha/Documents/dust_2019_ISRIC/',0)
+
