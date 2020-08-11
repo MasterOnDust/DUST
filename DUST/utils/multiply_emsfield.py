9977fdcc5f49046f64a58518f01355bbedc2dd20
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-08-11 14:09:30 +0200

    update multiflexpart flexdust

--- DUST/utils/multiply_emsfield.py
+++ DUST/utils/multiply_emsfield.py
@@ -26,15 +26,15 @@ def multi_flexpart_flexdust(path, nc_files, flexdust, point_spec, **kwargs):
     """
     Description:
     ===========
-        
-        Multiply flexpart emissions sensitivities with modelled dust emissions from FLEXDUST, 
+
+        Multiply flexpart emissions sensitivities with modelled dust emissions from FLEXDUST,
         and save the output to a new netcdf file.
 
     USAGE:
     =====
-        
+
         path : path to where the netcdf file should be created
-        nc_files : list containing the path to the FLEXPART netcdf files 
+        nc_files : list containing the path to the FLEXPART netcdf files
         flexdust_path : path to the directory containing the flexdust netcdf output
         point_spec : integer --> corrensponding to the release point in the flexpart netcdf file
 
@@ -51,8 +51,8 @@ def multi_flexpart_flexdust(path, nc_files, flexdust, point_spec, **kwargs):
     d0 = xr.open_dataset(nc_files[0], decode_times=False)
     d0 = _sel_location(d0,point_spec)
 
-    
-    #Copy variables 
+
+    #Copy variables
     lats = d0.latitude.values
     lons = d0.longitude.values
     ts = d0.time.values
@@ -82,8 +82,8 @@ def multi_flexpart_flexdust(path, nc_files, flexdust, point_spec, **kwargs):
     if isinstance(flexdust,xarray.Dataset):
         emsField=flexdust.Emission
     else:
-        emsField = DUST.read_flexdust_output(flexdust)['dset'].Emission  
-    
+        emsField = DUST.read_flexdust_output(flexdust)['dset'].Emission
+
     # Create netcdf outfile
 #     print(relcom[:-1])
     outFileName = path + '/' + '_'.join(relcom) + f_name + sdate +'.nc'
@@ -92,8 +92,8 @@ def multi_flexpart_flexdust(path, nc_files, flexdust, point_spec, **kwargs):
     except PermissionError:
         # If netcdf file exist delete old one
         os.remove(outFileName)
-        ncfile = Dataset(outFileName, 'w', format='NETCDF4') 
-    
+        ncfile = Dataset(outFileName, 'w', format='NETCDF4')
+
     #Setup attributes
     ncfile.title = 'Flexpart - Flexdust SSR'
     ncfile.history = "Created " + time.ctime(time.time())
@@ -103,67 +103,67 @@ def multi_flexpart_flexdust(path, nc_files, flexdust, point_spec, **kwargs):
 
 
     #Spatial dims
-    
+
     lat_dim = ncfile.createDimension('lat', dims['latitude'])
     lon_dim = ncfile.createDimension('lon', dims['longitude'])
     point_dim = ncfile.createDimension('npoint',1)
     #temporal dims
-    
+
     time_dim = ncfile.createDimension('time', None)
     btime_dim = ncfile.createDimension('btime', dims['time'])
-    
-    
+
+
     #Setup lon/lat (spatial variables)
-    
+
     lat = ncfile.createVariable('lat', 'f4', ('lat', ),**kwargs)
     lat.units = 'degrees_north'
     lat.long_name = 'latitude'
-    
+
     lon = ncfile.createVariable('lon', 'f4', ('lon', ), **kwargs)
     lon.units = 'degrees_east'
     lon.long_name = 'longitude'
-    
+
     #Set receptor location
     rellat = ncfile.createVariable('RELLAT', 'f4', ('npoint',),**kwargs)
     rellat.units = 'degrees_north'
     rellat.long_name = 'latitude_receptor'
-    
+
     rellon = ncfile.createVariable('RELLON', 'f4', ('npoint',), **kwargs)
     rellon.units = 'degrees_east'
     rellon.long_name = 'longitude_receptor'
-    
+
     lon[:] = lons
     lat[:] = lats
-    
+
     rellat[:] = d0.RELLAT1.values
     rellon[:] = d0.RELLNG1.values
-    
-    
-    # Setup temporal variable 
+
+
+    # Setup temporal variable
     btime = ncfile.createVariable('btime', 'i4', ('btime',), **kwargs)
     btime.units = 's'
     btime.long_name = 'seconds_since_release'
-    
+
     btime[:] = d0.time.values
-    
+
     time_var = ncfile.createVariable('time', 'f8', ('time',), **kwargs)
     time_var.units = "hours since 1980-01-01"
     time_var.long_name = 'time'
-    
+
     # Determind which kind of output should be created
 
-    
+
     field = ncfile.createVariable(f_name, 'f4', ('time', 'btime', 'lat', 'lon'), **kwargs)
     field.units = field_unit
     field.long_name = field_name
-    
+
     field.height = d0.height.values     #set the height of the lowest model output layer
     field.lon0 = d0.RELLNG1.values
     field.lat0 = d0.RELLAT1.values
     field.name_location = '-'.join(relcom)
     td = pd.to_timedelta(d0.time.values, unit='s')
     ncfile.close()
-    time_steps = []
+    #time_steps = []
     for n, nc_file in enumerate(nc_files):
         if n == 0:
             ncfile = Dataset(outFileName, 'a', format="NETCDF4")
@@ -171,14 +171,13 @@ def multi_flexpart_flexdust(path, nc_files, flexdust, point_spec, **kwargs):
         temp_ds = xr.open_dataset(nc_file, decode_times=False)
         temp_ds = _sel_location(temp_ds, point_spec)
         s_time = pd.to_datetime(temp_ds.iedate + temp_ds.ietime)
-        time_steps.append(date2num(s_time, time_var.units))
+        time_step = date2num(s_time, units = "hours since 1980-01-01")
         ems_sens = temp_ds.spec001_mr
         temp_ems = emsField.sel(time=s_time + td)
-        
+
         #multiply emission and emission sensitivity
-        temp_array = _multiply_emissions(temp_ems.values, ems_sens.values)  
-        ncfile['time'][:] = time_steps
-        print(outfield)
+        temp_array = _multiply_emissions(temp_ems.values, ems_sens.values)
+        ncfile['time'][n] = time_step
         outfield[n,:,:,:] = temp_array
 
         if n % 5== 0:
@@ -186,6 +185,7 @@ def multi_flexpart_flexdust(path, nc_files, flexdust, point_spec, **kwargs):
             print(n,'saved')
             ncfile = Dataset(outFileName, 'a', format="NETCDF4")
             outfield = ncfile[f_name]
+    ncfile.close()
     return outFileName
 
 @njit
@@ -210,23 +210,23 @@ def _sel_location(ds,pointspec):
 @dask.delayed
 def multi_flexpart_flexdust_dask(xr_flexpart, xr_flexdust, point_spec):
     """
-    
+
     DESCRIPTION
     ===========
-        
-        Multiply flexpart and flexdust xarray dataset in paraell using dask, and returns 
+
+        Multiply flexpart and flexdust xarray dataset in paraell using dask, and returns
         dask delayed object.
 
     USAGE
-    ===== 
+    =====
 
         xr_multi = multi_flexpart_flexdust_dask(xr_flexpart, xr_flexdust, point_spec)
 
         xr_flexpart : xarray dataset containing flexpart model output(s)
-        xr_flexdust : xarray dataset containing flexdust model output which is to be 
-                      matched and multiplied by the flexpart output 
+        xr_flexdust : xarray dataset containing flexdust model output which is to be
+                      matched and multiplied by the flexpart output
 
-    
+
     """
 
     # Create output dataset
@@ -234,7 +234,7 @@ def multi_flexpart_flexdust_dask(xr_flexpart, xr_flexdust, point_spec):
                 history = "Created " + time.ctime(time.time()),
                 reference = 'https://doi.org/10.5194/gmd-12-4955-2019, https://doi.org/10.1002/2016JD025482')
 
-    
+
     xr_flexpart = _sel_location(point_spec)
     d_out = xr.Dataset(data_vars={'RELCOM' : xr_flexpart.RELCOM,
                                 'RELLAT' : xr_flexpart.RELLAT1,
