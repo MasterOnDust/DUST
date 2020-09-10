7bddde51970c0e9ebadadbe5b4fe5b1135263dc1
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-09-10 11:00:12 +0000

    fix again

--- scripts/concat_output_serial.py
+++ scripts/concat_output_serial.py
@@ -102,7 +102,7 @@ def concat_output(ncfiles,pointspec,outpath, netCDF_kwargs={}):
 
     lon[:] = lons
     lat[:] = lats
-    height[:] = height
+    height[:] = heights
     rellat[:] = rel_lat
     rellon[:] = rel_lon
 
