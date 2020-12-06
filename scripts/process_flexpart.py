bc0e169e9bb55ff04339428205d6024d0ba9fce3
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-12-06 11:29:48 +0100

    include receptor name in filename

--- scripts/process_flexpart.py
+++ scripts/process_flexpart.py
@@ -1,23 +1,19 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 
 import xarray as xr
 import sys
 import argparse as ap
 import DUST
 import os
+import time
+
+
 """
 AUTHOR
 ======
     Ove Haugvaldstad
 
     ovehaugv@outlook.com
-
-TODO:
-    * Fix commandline parser
-    * Determine type of output Wet/Dry etc, can actually determine based on whats in the 
-      NETCDF file. 
-    * Make sure that output netcdf has the right chunk size. 
-    * Fix units of output
 
 """
 
@@ -28,14 +24,12 @@ if __name__ == "__main__":
         and save the output to a new NETCDF file file.""")
     parser.add_argument('path_flexpart', help='path to flexpart output')
     parser.add_argument('path_flexdust', help='path to flexdust output')
-    parser.add_argument('--psdfraction','--pf',help='particle size distrubtion fraction')
     parser.add_argument('--outpath', '--op', help='path to where output should be stored', default='./out')
     parser.add_argument('--x0' ,help = 'longitude of lower left corner of grid slice', default=None, type=int)
     parser.add_argument('--y0', help='latitude of lower left corner of grid slice', default=None, type=int)
     parser.add_argument('--x1', help='longitude of top right corner of grid slice', default=None, type=int)
     parser.add_argument('--y1', help='latidute of top right corner of grid slice', default=None, type=int)
     args = parser.parse_args()
-    locations = args.locations
     pathflexpart = args.path_flexpart
     pathflexdust = args.path_flexdust
     outpath = args.outpath
@@ -52,19 +46,19 @@ if __name__ == "__main__":
 
     if ind_receptor == 1:
         f_name = 'conc'
-        field_unit = 'kg/m^3'
+        field_unit = 'g/m^3'
         field_name = 'Concentration'
     elif ind_receptor == 4:
         f_name = 'drydep'
-        field_unit = 'kg/m^2 s'
+        field_unit = 'g/m^2 s'
         field_name = 'Dry depostion'
     elif ind_receptor == 3:
         f_name = 'wetdep'
-        field_unit = 'kg/m^2 s'
+        field_unit = 'g/m^2 s'
         field_name = 'Wet depostion'
     else:
         f_name = 'spec_mr'
-        field_unit = 'kg/m^3'
+        field_unit = 'g/m^3'
         field_name = 'Unknown'
 
     # only select surface sensitivity
@@ -100,25 +94,27 @@ if __name__ == "__main__":
     
     
     # Fill output Data arrary
-    
+    scale_factor = (1/height)*1000
+
     for i in range(len(ds.pointspec)):
-        out_data[i] = ds.spec001_mr.sel(pointspec=i) * flexdust_ds.Emission
+        out_data[i] = ds.spec001_mr.sel(pointspec=i) * flexdust_ds.Emission * scale_factor
     
     terminal_input = ' '.join(sys.argv)
 
     out_ds = xr.Dataset({f_name : out_data, 'surface_sensitvity':ds.spec001_mr, 'RELEND':ds.RELEND, 'RELSTART':ds.RELSTART, 
                         'ORO':ds.ORO, 'RELPART':ds.RELPART.sum(), 'RELZZ1':ds.RELZZ1[0],
                         'RELZZ2': ds.RELZZ2[0], 'RELLAT':ds.RELLAT1[0], 'RELLNG':ds.RELLNG1[0]
-                        ,'RELCOM':ds.RELCOM}, attrs=ds.attrs)
+                        ,'RELCOM':ds.RELCOM.astype('U35', copy=False)}, attrs=ds.attrs)
     ds.close()
+    receptor_name = str(out_ds.RELCOM[0].values).strip().split(' ')[1]
 
     out_ds.attrs['title'] = 'FLEXPART/FLEXDUST model output'
     out_ds.attrs['references'] = 'https://doi.org/10.5194/gmd-12-4955-2019, https://doi.org/10.1002/2016JD025482'
-    out_ds.attrs['history'] = out_ds.attrs['history'] + ' {}'.format(terminal_input)
+    out_ds.attrs['history'] = out_ds.attrs['history'] + '{} processed by {}'.format(time.ctime(time.time()),terminal_input)
     out_ds.attrs['varName'] = f_name
     shape_dset = out_ds[f_name].shape
     encoding = {'zlib':True, 'complevel':9, 'chunksizes' : (1,10, shape_dset[2], shape_dset[3]),
     'fletcher32' : False,'contiguous': False, 'shuffle' : False}
-    outFile_name = os.path.join(outpath,f_name + '_' + spec_com + out_ds.iedate +'-'+ out_ds.ibdate + '.nc')
+    outFile_name = os.path.join(outpath,f_name + '_' + receptor_name + '_' + spec_com + '_' + out_ds.iedate +'-'+ out_ds.ibdate + '.nc')
     
     out_ds.to_netcdf(outFile_name, encoding={f_name:encoding, 'surface_sensitvity':encoding})
