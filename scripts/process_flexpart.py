2f06654f004eaf7173d48e22b5b86dd4422841f7
Ove Haugvaldstad <ovehaugv@outlook.com>
2020-12-07 17:41:26 +0000

    make outdimmension consistent with previous method

--- scripts/process_flexpart.py
+++ scripts/process_flexpart.py
@@ -5,6 +5,8 @@ import sys
 import argparse as ap
 import DUST
 import os
+from netCDF4 import date2num, num2date
+import pandas as pd
 import time
 #from dask.distributed import Client, LocalCluster
 #import dask
@@ -49,19 +51,25 @@ if __name__ == "__main__":
     if ind_receptor == 1:
         f_name = 'conc'
         field_unit = 'g/m^3'
+        sens_unit = 's'
         field_name = 'Concentration'
     elif ind_receptor == 4:
         f_name = 'drydep'
         field_unit = 'g/m^2 s'
+        sens_unit = 'm'
         field_name = 'Dry depostion'
     elif ind_receptor == 3:
         f_name = 'wetdep'
         field_unit = 'g/m^2 s'
+        sens_unit = 'm'
         field_name = 'Wet depostion'
     else:
         f_name = 'spec_mr'
         field_unit = 'g/m^3'
+        sens_unit = 's'
         field_name = 'Unknown'
+
+    spec_com = ds.spec001_mr.attrs['long_name']
 
     # only select surface sensitivity
     ds = ds.isel(height=0)
@@ -74,45 +82,74 @@ if __name__ == "__main__":
     # select output domain
     ds = ds.sel(lon=slice(x0,x1), lat=slice(y0,y1))
 
+    # Determine simulation start
+    sim_start = pd.to_datetime(ds.iedate+ds.ietime)
+
+    # Determine size of backward time dimmension
+    lout_step = abs(ds.attrs['loutstep'])
+    btime_size = int(ds['LAGE']/lout_step*1e-9)
+    lout_step_h = int(lout_step/(60*60))
+    btime_array = np.arange(lout_step_h,btime_size*lout_step_h,lout_step_h)
+
+    # Create new time forward time dimmension
+    t0 = pd.to_datetime(ds.ibdate+ds.ibtime) + ds['LAGE'].values
+
+    time_a = np.arange(0,len(ds['pointspec'])*3,3)
+    time_var = xr.Variable('time',time_a, 
+                    attrs=dict(units='hours since {}'.format(t0.strftime('%Y-%m-%d %H:%S')),
+                    calendar="proleptic_gregorian"))
+
     # Read flexdust output
     flexdust_ds = DUST.read_flexdust_output(pathflexdust)['dset']
-    flexdust_ds = flexdust_ds.sel(time=ds.time)
-    flexdust_ds = flexdust_ds.sel(lon=slice(70,120), lat=slice(25,50))
+    flexdust_ds = flexdust_ds.sel(lon=slice(x0,x1), lat=slice(y0,y1))
 
     # Interpolate into same coordinates as FLEXPART
     flexdust_ds = flexdust_ds.interp_like(ds)
 
     # create output DataArray
     print('creating output array')
-    out_data = xr.DataArray(np.zeros((ds['spec001_mr'].shape), dtype=np.float32),
-            coords={'pointspec':ds.pointspec,'lon':ds['spec001_mr'].lon, 
-                            'lat':ds['spec001_mr'].lat, 'time':ds.time}, 
-                        dims={'pointspec':ds.pointspec,'time':ds.time,
-                        'lat':ds.lat,'lon':ds.lon}, attrs=ds.spec001_mr.attrs)
+    out_data = xr.DataArray(np.zeros((len(ds['pointspec']),btime_size,len(ds['lon']),len(ds['lat'])),dtype=np.float32),
+        dims=['time', 'btime', 'lon', 'lat'],
+        coords={'time':time_var,
+        'btime': ('btime',btime_array, dict(
+            long_name='time along back trajectory',
+            units='hours')), 
+        'lon':('lon',ds['spec001_mr'].lon), 
+        'lat':('lat',ds['spec001_mr'].lat)},
+        attrs=dict(
+            units=field_unit,
+            spec_com=ds.spec001_mr.attrs['long_name'],
+            long_name=field_name
+        ) 
+                        )
+    surface_sensitvity = out_data.copy()
+    surface_sensitvity.attrs['units'] = sens_unit
     print('created output array')
-    out_data.attrs['units'] = field_unit
-    spec_com = ds.spec001_mr.attrs['long_name']
-    out_data.attrs['spec_comment'] = spec_com
-    out_data.attrs['long_name'] = field_name
+
     
     
     # Fill output Data arrary
     scale_factor = (1/height)*1000
     #result=[]
-    for i in range(len(ds.pointspec)):
-        #result.append(dask.delayed(ds.spec001_mr.sel(pointspec=i) * flexdust_ds.Emission * scale_factor))
-        out_data[i] = ds.spec001_mr.sel(pointspec=i) * flexdust_ds.Emission * scale_factor
+    last_btime = out_data.btime[-1]
+    first_btime =out_data.btime[0]
+    time_units = out_data.time.units
+    for i in range(len(ds.out_data.time)):
+        date0 = num2date(out_data[i].time + first_btime, time_units).strftime('%Y%m%d%H%M')
+        date1 = num2date(out_data[i].time + last_btime, time_units).strftime('%Y%m%d%H%M')
+        temp_data = ds['spec001_mr'].sel(time=slice(date0, date1), pointspec=i).values
+        out_data[i] = temp_data*flexdust_ds['Emission'].sel(time=temp_data.time)*scale_factor
+        surface_sensitvity[i] = temp_data
+
     print('finish emsfield*sensitvity')
 
-    #out_data[:] = dask.compute(*result)
     terminal_input = ' '.join(sys.argv)
     
-    out_ds = xr.Dataset({f_name : out_data, 'surface_sensitivity' : ds['spec001_mr'] ,'RELEND':ds.RELEND, 'RELSTART':ds.RELSTART, 
+    out_ds = xr.Dataset({f_name : out_data, 'surface_sensitivity' : surface_sensitvity ,'RELEND':ds.RELEND, 'RELSTART':ds.RELSTART, 
                         'ORO':ds.ORO, 'RELPART':ds.RELPART.sum(), 'RELZZ1':ds.RELZZ1[0],
                         'RELZZ2': ds.RELZZ2[0], 'RELLAT':ds.RELLAT1[0], 'RELLNG':ds.RELLNG1[0]
                         ,'RELCOM':ds['RELCOM'].astype('U35', copy=False)}, attrs=ds.attrs)
-    #h=hpy()
-    #print(h.heap())
+
     flexdust_ds.close()
     ds.close()
     receptor_name = str(out_ds.RELCOM[0].values).strip().split(' ')[1]
