c3f03fa3df580382f24781a757a481110d846f47
Ove Haugvaldstad <ovewh@login2-nird-tos.nird.login.svc.cluster.local>
2021-01-07 17:19:34 +0100

    Made process_flexpart.py compatible with both version of the model output

--- scripts/DUST/process_flexpart.py
+++ scripts/DUST/process_flexpart.py
@@ -8,6 +8,7 @@ import os
 from netCDF4 import date2num, num2date
 import pandas as pd
 import time
+from IPython import embed
 #from dask.distributed import Client, LocalCluster
 #import dask
 """
@@ -36,8 +37,7 @@ def process_per_pointspec(dset,flexdust_ds, x0,x1,y0,y1, height=None):
     dset = dset.sel(lon=slice(x0,x1), lat=slice(y0,y1))
 
     #interpolate flexdust to match flexpart coordinates
-    flexdust_ds = flexdust_ds.interp_like(dset)
-
+    flexdust_ds = flexdust_ds.interp({'lon':dset.lon,'lat':dset.lat})    
 
     # Determine simulation start
     sim_start = pd.to_datetime(dset.iedate+dset.ietime)
@@ -47,7 +47,7 @@ def process_per_pointspec(dset,flexdust_ds, x0,x1,y0,y1, height=None):
     btime_size = int(dset['LAGE']/lout_step*1e-9)
     lout_step_h = int(lout_step/(60*60))
     btime_array = -np.arange(lout_step_h,btime_size*lout_step_h+lout_step_h,lout_step_h)
-
+    print(btime_array)
     # Create new time forward time dimmension
     t0 = pd.to_datetime(dset.ibdate+dset.ibtime) + dset['LAGE'].values
 
@@ -89,19 +89,36 @@ def process_per_pointspec(dset,flexdust_ds, x0,x1,y0,y1, height=None):
         surface_sensitivity[i] = temp_data.values    
     
     print('finish emsfield*sensitvity')
-
+    dset = dset.assign({
+        'RELLAT1' : dset['RELLAT1'][0],
+        'RELLNG1' : dset['RELLNG1'][0],
+        'RELZ1' : dset['RELZZ1'][0],
+        'RELZ2' : dset['RELZZ2'][0],
+        'RELPART' : dset['RELPART'].sum(keep_attrs=True)
+        })
+    
+    dset.attrs['ibdate'] = t0.strftime('%Y%m%d')
+    dset.attrs['ibtime'] = t0.strftime('%H%M%S')
+    receptor_name = str(dset.RELCOM[0].values).strip().split(' ')[1:]
+    dset.attrs['relcom'] = receptor_name
     return dset,out_data, surface_sensitivity
 
-def process_per_timestep(dset, x0,x1,y0,y1, height=None):
+def process_per_timestep(dset, flexdust_ds,x0,x1,y0,y1, height=None):
     dset = dset.sel(lon=slice(x0,x1), lat=slice(y0,y1))
+    
+    #interpolate flexdust to match flexpart coordinates
+    flexdust_ds = flexdust_ds.interp({'lon':dset.lon,'lat':dset.lat})    
+    
     if height == None:
-        dset = dset.height.values
+        
+        height = dset.height.values
     else:
         height = height
+    print(height)
     scale_factor = (1/height)*1000
     print('creating output array')
     out_data = xr.zeros_like(dset['spec001_mr'])
-    for i in range(out_data.time):
+    for i in range(len(out_data.time)):
         
         temp_data = dset['spec001_mr'].isel(time=i)
         time_steps = temp_data.time + temp_data.btime 
@@ -109,6 +126,13 @@ def process_per_timestep(dset, x0,x1,y0,y1, height=None):
         out_data[i] = temp_data.values*emission_field.values*scale_factor
     print('finish emsfield*sensitvity')
     surface_sensitivity = dset['spec001_mr']
+    iedate_stamp = dset.time[-1]
+    ibdate_stamp = dset.time[0]
+    dset.attrs['iedate'] = str(iedate_stamp.dt.strftime('%Y%m%d').values)
+    dset.attrs['ietime'] = str(iedate_stamp.dt.strftime('%H%M%S').values)
+
+    dset.attrs['ibdate'] = str(ibdate_stamp.dt.strftime('%Y%m%d').values)
+    dset.attrs['ibtime'] = str(ibdate_stamp.dt.strftime('%H%M%S').values)
     return dset, out_data, surface_sensitivity
 
 
@@ -121,21 +145,22 @@ def create_output(out_data, surface_sensitivity, dset):
     terminal_input = ' '.join(sys.argv)
     
     out_ds = xr.Dataset({f_name : out_data, 'surface_sensitivity' : surface_sensitivity ,'RELEND':dset.RELEND, 'RELSTART':dset.RELSTART, 
-                        'ORO':dset.ORO, 'RELPART':dset.RELPART.sum(), 'RELZZ1':ds.RELZZ1[0],
-                        'RELZZ2': dset.RELZZ2[0], 'RELLAT':dset.RELLAT1[0], 'RELLNG':dset.RELLNG1[0]
+                        'RELPART':dset.RELPART, 'RELZZ1':ds.RELZZ1,
+                        'RELZZ2': dset.RELZZ2, 'RELLAT':dset.RELLAT1, 'RELLNG':dset.RELLNG1
                         }, attrs=dset.attrs)
 
-    dset[f_name].attrs['units'] = field_unit
-    dset[f_name].attrs['long_name'] = field_name
-    dset['surface_sensitivity'].attrs['units'] = sens_unit
-    receptor_name = str(out_ds.RELCOM[0].values).strip().split(' ')[1]
+    out_ds[f_name].attrs['units'] = field_unit
+    out_ds[f_name].attrs['long_name'] = field_name
+    out_ds['surface_sensitivity'].attrs['units'] = sens_unit
 
     out_ds.attrs['title'] = 'FLEXPART/FLEXDUST model output'
     out_ds.attrs['references'] = 'https://doi.org/10.5194/gmd-12-4955-2019, https://doi.org/10.1002/2016JD025482'
     out_ds.attrs['history'] = '{} processed by {}, '.format(time.ctime(time.time()),terminal_input) + out_ds.attrs['history']
     out_ds.attrs['varName'] = f_name
-
+    receptor_name = out_ds.attrs['relcom'][0]
+    spec_com = out_ds.attrs['relcom'][1]
     file_name = f_name + '_' + receptor_name + '_' + spec_com + '_' + out_ds.ibdate +'-'+ out_ds.iedate + '.nc'
+ 
     out_ds.attrs['filename'] = file_name
 
     
@@ -177,7 +202,7 @@ if __name__ == "__main__":
     parser.add_argument('--y0', help='latitude of lower left corner of grid slice', default=None, type=int)
     parser.add_argument('--x1', help='longitude of top right corner of grid slice', default=None, type=int)
     parser.add_argument('--y1', help='latidute of top right corner of grid slice', default=None, type=int)
-    parser.add_argument('--height', help='height of lowest outgrid height')
+    parser.add_argument('--height', help='height of lowest outgrid height', default=None, type=int)
     args = parser.parse_args()
 
     pathflexpart = args.path_flexpart
@@ -194,14 +219,17 @@ if __name__ == "__main__":
     flexdust_ds = flexdust_ds.sel(lon=slice(x0,x1), lat=slice(y0,y1))
     # Check whether output is per time step or per release?
     ds = xr.open_dataset(pathflexpart)
-    if 'pointspec' in ds.data_vars:
+    if 'pointspec' in ds.dims:
+        print('per receptor point')
         ds, out_data, surface_sensitivity = process_per_pointspec(ds, flexdust_ds, x0, x1, y0, y1, height=height)
         ds.attrs['relcom'] = str(ds.RELCOM[0].values.astype('U35')).strip().split(' ')[1:]
+
     else:
+        print('per timestep')
         ds, out_data, surface_sensitivity = process_per_timestep(ds, flexdust_ds, x0, x1, y0, y1, height=height) 
     
     out_ds = create_output(out_data,surface_sensitivity,ds)
-
+    
     flexdust_ds.close()
     ds.close()
     spec_com = ds.spec001_mr.attrs['long_name']
