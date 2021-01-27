629c9fa7ff896810cec36d75855ab827563d5801
Ove Haugvaldstad <ovehaugv@outlook.com>
2021-01-27 11:06:50 +0100

    Added shell script for concatenating files for 2micon conc simulation

--- DUST_scripts/concat_output.py
+++ DUST_scripts/concat_output.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 import os
-from IPython import embed
-from dask.distributed import Client, LocalCluster 
 from DUST.read_data import read_multiple_flexpart_outputs
-from DUST.utils.utils import arg_parser
-from distributed import client
-import pandas as pd
-import shutil
 import numpy as np
 import sys
 import argparse as ap
-from dask.diagnostics import ProgressBar
-import time
+from IPython import embed
+
 def slice_data(dset, x0, x1, y0, y1):
     dset = dset.sel(lon=slice(x0,x1),lat=slice(y0,y1))
     spatial_attrs_names = ['outlon0', 'outlon1', 'outlat0', 'outlat1']
@@ -26,33 +20,37 @@ def slice_data(dset, x0, x1, y0, y1):
     return dset
 
 def prepare_output_dataset(dset, height, point, fileName=None):
-
     dset = dset.squeeze()
-    d0 = dset.time[0].dt.strftime('%Y%m%d%-%H')
-    d1 = dset.time[-1].dt.strftime('%Y%m%d%-%H')
+    d0 = dset.time[0].dt.strftime('%Y%m%d')
+    d1 = dset.time[-1].dt.strftime('%Y%m%d')
     Loc_name = str(dset.RELCOM.values).strip().split(' ')
-    if fileName==None:
-        file_name = "{}_{}_{}_{}.nc".format('_'.join(Loc_name),h, d0, d1) 
-    else:
-        file_name=fileName
-    dset = dset.drop_vars('RELCOM')
 
+    file_name = "{}_{}_{}_{}.nc".format('_'.join(Loc_name),height,
+                                        d0.values, d1.values)
     dset.attrs['relcom'] = Loc_name
     dset[dset.varName] = dset[dset.varName].astype(np.float32)
-    dset.attrs['source'] = dset.attrs['source'] + ' '.join(nc_files)  
-    dset.attrs['filename']=file_name 
-    return dset 
+    dset.attrs['source'] = dset.attrs['source'] + ' '.join(nc_files)
+    dset.attrs['filename']=file_name
+    return dset
 
 if __name__ == "__main__":
     parser = ap.ArgumentParser(description='Concatenate FLEXPART simulations')
-    parser.add_argument('--x0' ,help = 'longitude of lower left corner of grid slice', default=None, type=int)
-    parser.add_argument('--y0', help='latitude of lower left corner of grid slice', default=None, type=int)
-    parser.add_argument('--x1', help='longitude of top right corner of grid slice', default=None, type=int)
-    parser.add_argument('--y1', help='latidute of top right corner of grid slice', default=None, type=int)
-    parser.add_argument('--height', help='which levels to concatenate output for', type=int, default=None)
+    parser.add_argument('--x0' ,help = 'longitude of lower left corner of grid slice',
+                        default=None, type=int)
+    parser.add_argument('--y0', help='latitude of lower left corner of grid slice',
+                        default=None, type=int)
+    parser.add_argument('--x1', help='longitude of top right corner of grid slice',
+                        default=None, type=int)
+    parser.add_argument('--y1', help='latidute of top right corner of grid slice',
+                        default=None, type=int)
+    parser.add_argument('--height', help='which levels to concatenate output for',
+                        type=int, default=None)
     parser.add_argument('paths', help='Paths to netcdf files to be concatenated',nargs='+')
-    parser.add_argument('--outpath', '--op', help='Where the output should be stored', default='.')
-    parser.add_argument('location', '--loc', help='Number corresponding to specific location in dataset', default=None, type=int)
+    parser.add_argument('--outpath', '--op', help='Where the output should be stored',
+                        default='.')
+    parser.add_argument('--location', '--loc',
+                        help='Number corresponding to specific location in dataset',
+                        default=None, type=int)
     args = parser.parse_args()
     location = args.location
     nc_files = args.paths
@@ -62,9 +60,11 @@ if __name__ == "__main__":
     y1 = args.y1
     y0 = args.y0
     height = args.height
-    
     # Load the netCDF files
-    dset = read_multiple_flexpart_outputs(nc_files, height=height, location=location, parallel=False)
+    print(len(nc_files))
+    dset = read_multiple_flexpart_outputs(nc_files, height=height,
+                                          location=location, parallel=True,
+                                          chunks={'pointspec':1, 'height':1})
     dset = slice_data(dset, x0, x1, y0,y1)
     dset=prepare_output_dataset(dset, height, location)
     shape_dset = dset[dset.varName].shape
@@ -73,14 +73,12 @@ if __name__ == "__main__":
         'chunksizes':(1,shape_dset[1], shape_dset[2], shape_dset[3]),
     }
     if outpath==None:
-        outfile_path = dset['filename']
+        outfile_path = dset.attrs['filename']
     else:
-        outfile_path = outpath
+        outfile_path = os.path.join(outpath, dset.attrs['filename'])
 
-    dset = dset.drop_vars('RELCOM')
 
     dset[dset.varName] = dset[dset.varName].astype(np.float32)
-    dset.attrs['source'] = dset.attrs['source'] + ' '.join(nc_files)    
-    out_file = dset.to_netcdf(outfile_path,encoding={dset.varName:encoding}, 
+    dset.to_netcdf(outfile_path,encoding={dset.varName:encoding},
                     unlimited_dims=['time'])
 
---
+++ DUST_scripts/concat_output.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+##############################################################################
+# This is for concatenating FLEXPART output from simulations setup according #
+# to multiple receptor location in the RELEASE FILE, and new simulation      #
+# every timestep.                                                            #
+# Data which needs to concatenated into one file before subsequent analysis  #
+# can be done.                                                               #
+##############################################################################
+
+
+shopt -s extglob
+shopt -s globstar
+
+
+
+
+concat_output () {
+    N=4
+    output_dir=$3
+    [[ -d ${output_dir} ]] || mkdir ${output_dir} 
+
+    year=$2
+    temp_dir="$1$2"
+    for month in {3..5}; do
+        if [[ ${month} -eq 3 ]]; then
+            for p in {0..6}; do
+                ((i=i%N)); ((i++==0)) && wait
+                python concat_output.py ${temp_dir}/${year}0${month}[06-09,10-31]*/output/*.nc --x0 73 \
+                --x1 115 --y0 30 --y1 50 --op ${output_dir} --location $p --height 100& 
+            done
+        else 
+            for p in {0..6}; do
+                ((i=i%N)); ((i++==0)) && wait
+                python concat_output.py ${temp_dir}/${year}0${month}[01-09,10-31]*/output/*.nc --x0 73 \
+                --x1 115 --y0 30 --y1 50 --op ${output_dir} --location $p --height 100&
+            done 
+        fi
+    done
+}
+
+TARGETDIR_CONC='/projects/NS2806K/ovewh/tracing_the_winds/flexpart/FLEXPART_spring/emission_sensitivities/Conc/2micron/'
+
+
+OUTPUT_DIR_CONC='/projects/NS2806K/ovewh/tracing_the_winds/flexpart/FLEXPART_spring/emission_sensitivities/Conc/2micron/surface_sensitivities_test'
+[[ -d ${OUTPUT_DIR_CONC} ]] || mkdir ${OUTPUT_DIR_CONC} 
+# Years are in per timestep format
+TARGETYEARS_CONC=(1999 2000 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010
+    2011 2012 2013 2014 2016 2017 2018 2019)
+
+
+
+TARGETDIR_DRYDEP='/projects/NS2806K/ovewh/tracing_the_winds/flexpart/FLEXPART_spring/emission_sensitivities/DryDep/20micron/'
+OUTPUT_DIR_DRYDEP='/projects/NS2806K/ovewh/tracing_the_winds/flexpart/FLEXPART_spring/emission_sensitivities/DryDep/20micron/surface_sensitivities_test'
+TARGETYEARS_DRYDEP=2019
+
+[[ -d ${OUTPUT_DIR_DRYDEP} ]] || mkdir ${OUTPUT_DIR_DRYDEP} 
+
+
+#concat_output ${TARGETDIR_DRYDEP} ${TARGETYEARS_DRYDEP} \
+#${OUTPUT_DIR_DRYDEP}/${TARGETYEARS_DRYDEP}
+
+for year in ${TARGETYEARS_CONC[@]}; do
+    concat_output ${TARGETDIR_CONC} ${year} ${OUTPUT_DIR_CONC}/${year}
+    \ ${OUTPUT_DIR_CONC/${year}
+}
+done
+
