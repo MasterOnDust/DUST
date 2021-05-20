4d5ee35608bf06e9b510ada027df030a5977e311
Ove <ovehaugv@outlook.com>
2021-05-20 15:19:26 +0200

    small improvement of plotting

--- DUST/plot/maps.py
+++ DUST/plot/maps.py
@@ -3,11 +3,11 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import cartopy.io.img_tiles as cimgt
 import cartopy.io.shapereader as shpreader
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-from cartopy.mpl.gridliner import Gridliner
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 import matplotlib.pyplot as plt
 import matplotlib.patheffects as PathEffects
 import matplotlib.colors as mcolors
+from matplotlib.ticker import ScalarFormatter,AutoMinorLocator
 import matplotlib.ticker as mticker
 
 from shapely.geometry import LineString, MultiLineString
@@ -129,7 +129,8 @@ def map_terrain_china(ax=None):
 
 
 
-def map_china(ax=None, lakes_and_rivers=False):
+def map_china(ax=None, lakes_and_rivers=False, xticks =[80, 90, 100, 110],
+                yticks=[30, 35, 40, 45, 50]):
     """
     DESCRIPTION
     ===========
@@ -168,14 +169,25 @@ def map_china(ax=None, lakes_and_rivers=False):
                             alpha=.8, linestyle=':')
 
     ax.add_feature(boundary_10m, edgecolor='gray')
+    
+    lon_formatter = LongitudeFormatter(zero_direction_label=True)
+    lat_formatter = LatitudeFormatter()
+    ax.set_xticks(xticks, crs=ccrs.PlateCarree())
+    ax.set_yticks(yticks, crs=ccrs.PlateCarree())
+    ax.xaxis.set_major_formatter(lon_formatter)
+    ax.yaxis.set_major_formatter(lat_formatter)
+#     ax.yaxis.set_major_formatter(ScalarFormatter())
+    ax.yaxis.set_minor_locator(  AutoMinorLocator(4))
+    ax.xaxis.set_minor_locator(  AutoMinorLocator(4))
+
     ax.coastlines('10m', color='gray', alpha=0.8)
     ax.set_extent([75,115, 30, 50], crs=ccrs.PlateCarree())
-    gl = ax.gridlines(crs = ccrs.PlateCarree(), draw_labels = True, linestyle ='--', xformatter=LONGITUDE_FORMATTER,
-                   yformatter=LATITUDE_FORMATTER)
-    gl.xlocator = mticker.FixedLocator([80, 85, 90, 95, 100, 105, 110])
-    gl.ylocator = mticker.FixedLocator([30, 35, 40, 45])
-    gl.top_labels = False
-    gl.right_labels = False
+    # gl = ax.gridlines(crs = ccrs.PlateCarree(), draw_labels = True, linestyle ='--', xformatter=LONGITUDE_FORMATTER,
+    #                yformatter=LATITUDE_FORMATTER)
+    # gl.xlocator = mticker.FixedLocator([80, 85, 90, 95, 100, 105, 110])
+    # gl.ylocator = mticker.FixedLocator([30, 35, 40, 45])
+    # gl.top_labels = False
+    # gl.right_labels = False
     if lakes_and_rivers:
         ax.add_feature(cr.feature.RIVERS)
         ax.add_feature(cr.feature.LAKES)
